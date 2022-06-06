8b7565013fefc5d5aea8c01603bbebab4fde5a47
BlackRoadCode <blackroadcode@gmail.com>
2022-06-06 16:31:23 -0500

    end of section 19

--- curso-u-master-algoritmos/section 19/07-fatten-numbers.cpp
+++ curso-u-master-algoritmos/section 19/07-fatten-numbers.cpp
@@ -1,16 +1,28 @@
 #include <iostream>
-// #include <cmath>
 using namespace std;
+
+int maximo(int a, int b){
+    if ( a >= b ) return a;
+    else return b;
+}
 
 int fatten( int x ){
     if( x < 10 ) return x;
     else{
+        // al hacer módulo 10 de x, siempre obtenemos el último dígito del número
         int n = x % 10;
-        
+        int fn = fatten( x / 10 ) % 10;
+        int valor = maximo(n, fn);
+
+        return 10 * fatten( x / 10 ) + valor;
     }
 }
 
 int main() {
+    int x;
+    while ( cin >> x ){
+        cout << fatten( x ) << endl;
+    }
     
     return 0;
 }
