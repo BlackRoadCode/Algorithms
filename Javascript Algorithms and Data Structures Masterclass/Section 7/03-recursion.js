351a7ab65a78e381e1830b9e6930b0484d501cb6
BlackRoadCode <blackroadcode@gmail.com>
2022-06-24 13:08:34 -0500

    update section 7

---
+++ Javascript Algorithms and Data Structures Masterclass/Section 7/03-recursion.js
@@ -0,0 +1,24 @@
+function sumRange( num ){
+    if( num == 1 ) return 1;
+    return num + sumRange( num - 1 );
+}
+
+console.log(sumRange(10));
+
+19
+
+// Ejemplo de funcionamiento
+// sumRange( 4 )
+//     return 4 + sumRange( 3 )
+//                     return 3 + sumRange( 2 )
+//                                     return 2 + sumRange( 1 )
+//                                                     return 1;
+                                                
+//                                                 devuelve 1
+//                                 devuelve 3
+//                 devuelve 6
+// devuelve 10
+
+
+
+
