5e803d1cc6254ebc920f8c16c79f1dd6c1a37d14
BlackRoadCode <blackroadcode@gmail.com>
2022-06-26 13:57:48 -0500

    update section 8

--- Javascript Algorithms and Data Structures Masterclass/Section 8/02-factorial.js
+++ Javascript Algorithms and Data Structures Masterclass/Section 8/02-factorial.js
@@ -1,9 +1,12 @@
 
-function factorial(){
-    
+function factorial(num) {
+    if (num == 0) return 1;
+    return num *= factorial( --num );
 }
 
-//factorial(1) // 1
-// factorial(2) // 2
-// factorial(4) // 24
-// factorial(7) // 5040
+console.log(factorial(0)); // 1
+console.log(factorial(1)); // 1
+console.log(factorial(2)); // 2
+console.log(factorial(4)); // 24
+console.log(factorial(7)); // 5040
+console.log(factorial(14)); // 87178291200
---
+++ Javascript Algorithms and Data Structures Masterclass/Section 8/04-recursiveRange.js
@@ -0,0 +1,8 @@
+
+function recursiveRange(){
+    
+}
+
+// SAMPLE INPUT/OUTPUT
+// recursiveRange(6) // 21  - 6 + 5 + 4 + 3 + 2 + 1
+// recursiveRange(10) // 55 
