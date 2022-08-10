9da27f0e1f9e448ed32f8c5a854bb3d3f58ec604
BlackRoadCode <blackroadcode@gmail.com>
2022-08-10 15:21:48 -0500

    push method in singly linked list

---
+++ Javascript Algorithms and Data Structures Masterclass/Section 19/01-singly-linked-list.js
@@ -0,0 +1,38 @@
+class Node {
+    constructor( value ) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push( val ) {
+        let node = new Node( val );
+
+        if ( !this.head ) {
+            this.head = node;
+            this.tail = this.head;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+
+        this.length++;
+        return this;
+    }
+
+}
+
+let list = new SinglyLinkedList();
+list.push('hello');
+list.push('world');
+list.push('since');
+list.push('javascript');
+
+console.log(list);
