f13c807ebc76fc699223d932baba612d18323139
BlackRoadCode <blackroadcode@gmail.com>
2022-08-16 15:22:53 -0500

    added two methods to the SinglyLinkedList class

--- Javascript Algorithms and Data Structures Masterclass/Section 19/01-singly-linked-list.js
+++ Javascript Algorithms and Data Structures Masterclass/Section 19/01-singly-linked-list.js
@@ -27,12 +27,86 @@ class SinglyLinkedList {
         return this;
     }
 
+    pop(){
+        if( !this.head ) return undefined;
+
+        let current = this.head;
+        let newTail = current;
+
+        while( current.next ){
+            newTail = current;
+            current = current.next;
+        }
+
+        this.tail = newTail;
+        this.tail.next = null;
+        this.length--;
+        if( this.length === 0 ){
+            this.head = null;
+            this.tail = null;
+        }
+        return current;    
+    }
+
+    shift(){
+        if( !this.head ) return undefined;
+
+        let current = this.head;
+        this.head = current.next;
+        this.length--;
+        if( this.length === 0 ) this.tail = null;
+        
+        return current;
+    }
+
+    unshift( val ){
+        let node = new Node( val );
+
+        if ( !this.head ){
+            this.head = node;
+            this.tail = this.head;
+        } else {
+            node.next = this.head;
+            this.head = node;
+        }
+
+        this.length++;
+        return this;
+    }
+
+    get( index ){
+        if ( index < 0 || index >= this.length ) return null;
+
+        let current = this.head;
+
+        for ( let i = 0; i < index; i++) {
+            current = current.next;
+        }
+
+        return current;
+    }
+    
+    // get( index ){
+    //     if ( index < 0 || index >= this.length ) return null;
+
+    //     let counter = 0;
+    //     let current = this.head;
+
+    //     while ( counter !== index) {
+    //         current = current.next;
+    //         counter++;
+    //     }
+
+    //     return current;
+    // }
+
 }
 
 let list = new SinglyLinkedList();
 list.push('hello');
 list.push('world');
 list.push('since');
-list.push('javascript');
 
 console.log(list);
+
+console.log( list.get(1) );
