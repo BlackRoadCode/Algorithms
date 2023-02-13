2c67a5e6a81899495c520833abcad69b8fa503aa
BlackRoadCode <blackroadcode@gmail.com>
2023-02-13 17:20:18 -0600

    change

--- Javascript Algorithms and Data Structures Masterclass/Section 19/01-singly-linked-list.js
+++ Javascript Algorithms and Data Structures Masterclass/Section 19/01-singly-linked-list.js
@@ -172,13 +172,5 @@ class SinglyLinkedList {
 
 let list = new SinglyLinkedList();
 list.push(1);
-list.push(2);
-list.push(3);
-list.push(4);
-list.push(5);
-
-console.log(list.print());
-
-list.reverse()
 
 console.log(list.print());
